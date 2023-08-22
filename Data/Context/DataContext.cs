13733822d7d9abce5287731e1f9a4c0be74bd600
andrewschwabe <schwabea530@gmail.com>
2023-08-22 04:57:09 -0500

    Set db props as camel case

--- Data/Context/DataContext.cs
+++ Data/Context/DataContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Data.Context
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions options): base(options) 
+        public DataContext(DbContextOptions options) : base(options)
         {
 
         }
@@ -17,6 +18,21 @@ namespace Data.Context
                 .HasNoDiscriminator()
                 .ToContainer(nameof(TeamMember.Model.TeamMember))
                 .HasPartitionKey(p => p.Id);
+
+            modelBuilder.Entity<TeamMember.Model.TeamMember>().CamelCasePropertyNames();
+        }
+    }
+
+    public static class EntityTypeBuilderExtensions
+    {
+        public static void CamelCasePropertyNames<T>(this EntityTypeBuilder<T> entity)
+            where T : class
+        {
+            foreach (var property in typeof(T).GetProperties())
+            {
+                entity.Property(property.Name)
+                    .ToJsonProperty(char.ToLowerInvariant(property.Name[0]) + property.Name.Substring(1));
+            }
         }
     }
 }
--- Data/TeamMember/Model/TeamMember.cs
+++ Data/TeamMember/Model/TeamMember.cs
@@ -1,10 +1,16 @@
-﻿namespace Data.TeamMember.Model
+﻿using Microsoft.Azure.Documents;
+using System.Text.Json.Serialization;
+
+namespace Data.TeamMember.Model
 {
-    public class TeamMember
+    public class TeamMember 
     {
         public string Id { get; set; }
+        
         public string FirstName { get; set; }
+        
         public string LastName { get; set; }
+        
         public string EmailAddress { get; set; }
     }
 }
