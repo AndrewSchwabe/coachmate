4c068d26453149daf844f4336e200313defdc8a7
andrewschwabe <schwabea530@gmail.com>
2023-10-25 04:37:03 -0500

    Calendar add dialog working

--- Client/Models/Schedule/AddScheduleEventModel.cs
+++ Client/Models/Schedule/AddScheduleEventModel.cs
@@ -10,9 +10,15 @@ namespace Client.Models.Schedule
         public string Title { get; set; }
 
         [Required]
-        public DateTime? Start { get; set; } = DateTime.Now;
+        public DateTime? Start { get; set; } = DateTime.Today;
+
         [Required]
-        public TimeSpan? StartTime { get; set; }
-        public TimeSpan? End { get; set; }
+        public TimeSpan? StartTime { get; set; } = DateTime.Now.TimeOfDay;
+        
+        [Required]
+        public DateTime? End { get; set; } = DateTime.Today;
+
+        [Required]
+        public TimeSpan? EndTime { get; set; } = DateTime.Now.TimeOfDay.Add(TimeSpan.FromHours(1));
     }
 }
